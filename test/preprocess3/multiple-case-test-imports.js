781397f8a4f513e4c9021b4e6b8842ac88ee431d
Tetsuya Mori <t2y3141592@gmail.com>
2019-01-07 13:54:41 +0900

    3.0.0-pre.7 with [WIP][test] Issue #60 Add test/preprocess3

---
+++ test/preprocess3/multiple-case-test-imports.js
@@ -0,0 +1,22 @@
+import './fake-server.js';
+import './test-runner.js';
+import '../../i18n-element.js';
+import '../../define-element.js';
+import '../../i18n-dom-bind.js';
+import './multiple-case/multiple-element.js';
+import './multiple-case-test-suites.js';
+
+/**
+@license https://github.com/t2ym/i18n-behavior/blob/master/LICENSE.md
+Copyright (c) 2016, Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
+*/
+/*
+<link rel="import" href="../../i18n-element.html">
+<link rel="import" href="../../define-element.html">
+*/
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+;
