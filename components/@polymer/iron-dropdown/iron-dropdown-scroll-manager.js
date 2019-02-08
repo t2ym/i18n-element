44c6801e9ded903e382f0d9d182a7ddd7b9a5921
Tetsuya Mori <t2y3141592@gmail.com>
2019-02-08 21:27:48 +0900

    [gh-pages] Remove old gh-pages

--- components/@polymer/iron-dropdown/iron-dropdown-scroll-manager.js
+++
@@ -1,2 +0,0 @@
-import { IronScrollManager } from '../iron-overlay-behavior/iron-scroll-manager.js';
-export { IronScrollManager as IronDropdownScrollManager };
--- components/i18n-format/import-plurals.js
+++
@@ -1,1 +0,0 @@
-import '../make-plural/plurals.js';
