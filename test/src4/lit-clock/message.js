7a87401a30a150020a8c5a86c823d55d1f925f74
Tetsuya Mori <t2y3141592@gmail.com>
2019-02-23 22:02:04 +0900

    [test] Add some illegal and abnormal cases

--- test/src4/lit-clock/message.js
+++ test/src4/lit-clock/message.js
@@ -3,14 +3,14 @@
 Copyright (c) 2018, Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
 */
 
-import {html, bind} from '../../../i18n.js';
+import {html, bind, i18n} from '../../../i18n.js';
 
 export const binding = bind('get-message', import.meta); // the name must be unique among the app
 
 let mutatingMessage = '';
 setInterval(() => {
   mutatingMessage = Date.now();
-  binding.element.fire('lang-updated'); // Any events other than 'lang-updated' will do as long as the user (lit-clock) can detect them
+  binding.element.fire('lang-updated', null); // Any events other than 'lang-updated' will do as long as the user (lit-clock) can detect them
 }, 500);
 const getMutatingMessage = () => {
   return mutatingMessage;
@@ -22,6 +22,30 @@ export const getMessage = () => {
 
 export const getMessage2 = () => {
   return html`${bind('get-message2', import.meta)}<div>message 2</div>`;
+}
+
+binding.element.resolveUrl('');
+
+bind('', null, () => [], {});
+
+class DummyElement extends i18n(class DummyMixinElement extends HTMLElement {
+  static get observedAttributes() { return ['attr']; }
+  attributeChangedCallback(name, oldValue, newValue) {} }) {}
+customElements.define(DummyElement.is, DummyElement);
+document.createElement(DummyElement.is).setAttribute('attr', 'value');
+
+try {
+  html(['', 'hello'], bind('invalid-template'), 1, 2, 3);
+}
+catch (e) {
+  console.log(e);
+}
+
+try {
+  html`${bind('invalid-message', import.meta)}<div>hello{{parts.1:invalid}}</div>`;
+}
+catch (e) {
+  console.log(e);
 }
 
 /* broken bindings */
