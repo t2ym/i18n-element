fc0355fd074422bbbe6944bad45350a92889fe1a
Tetsuya Mori <t2y3141592@gmail.com>
2019-03-04 19:01:38 +0900

    [test] Issue #63 Remove polyfill.js from coverage

--- wct.conf.js
+++ wct.conf.js
@@ -49,7 +49,6 @@ module.exports = {
         "**/define-element.js",
         "**/define-element-base.js",
         "**/i18n.js",
-        "**/polyfill.js",
         "**/i18n-behavior/i18n-behavior.js",
         "**/i18n-behavior/i18n-attr-repo.js",
         "**/i18n-behavior/i18n-preference.js"
