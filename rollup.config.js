0d4d855a0c7f884a1ce08e17f3e47bafa360f16b
Tetsuya Mori <t2y3141592@gmail.com>
2019-03-10 22:34:25 +0900

    [skip ci] Issue #63 Update rollup.config.js for npm run size to support import.meta in bundled modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import resolve from 'rollup-plugin-node-resolve';
 import sizes from 'rollup-plugin-sizes';
 import filesize from 'rollup-plugin-filesize';
-import minify from 'rollup-plugin-babel-minify';
+import babel from 'rollup-plugin-babel';
+import minifyPreset from 'babel-preset-minify';
 
 export default {
   input: 'i18n.js',
@@ -13,38 +14,41 @@ export default {
     resolve({
       modulesOnly: true,
     }),
-    minify({
-      // Options from polymer-build/src/js-transform.ts
-      // Disable the minify-constant-folding plugin because it has a bug relating
-      // to invalid substitution of constant values into export specifiers:
-      // https://github.com/babel/minify/issues/820
-      evaluate: false,
-
-      // TODO(aomarks) Find out why we disabled this plugin.
-      simplifyComparisons: false,
-
-      // Prevent removal of things that babel thinks are unreachable, but sometimes
-      // gets wrong: https://github.com/Polymer/tools/issues/724
-      deadcode: false,
-
-      // Disable the simplify plugin because it can eat some statements preceeding
-      // loops. https://github.com/babel/minify/issues/824
-      simplify: false,
-
-      // This is breaking ES6 output. https://github.com/Polymer/tools/issues/261
-      mangle: false,
-
+    babel({
+      sourceMaps: false,
+      comments: false,
       plugins: [
         '@babel/plugin-syntax-object-rest-spread',
         '@babel/plugin-syntax-async-generators',
-        //'@babel/plugin-syntax-dynamic-import', // enabled by default
+        '@babel/plugin-syntax-dynamic-import',
         '@babel/plugin-syntax-import-meta',
+        // rollup rewrites import.meta.url, but makes them point to the file location after bundling
+        // we want the location before bundling
+        'bundled-import-meta',
       ],
+      presets: [
+        minifyPreset({}, {
+          // Options from polymer-build/src/js-transform.ts
+          // Disable the minify-constant-folding plugin because it has a bug relating
+          // to invalid substitution of constant values into export specifiers:
+          // https://github.com/babel/minify/issues/820
+          evaluate: false,
 
-      comments: false, // indicates if comments should be preserved in source;
-      banner: undefined, // the comment which should be prepended to the transformed bundle;
-      bannerNewLine: false, // indicates if the banner comment should be followed by a new line;
-      sourceMap: false, // indicates if sourcemap should be generated;
+          // TODO(aomarks) Find out why we disabled this plugin.
+          simplifyComparisons: false,
+
+          // Prevent removal of things that babel thinks are unreachable, but sometimes
+          // gets wrong: https://github.com/Polymer/tools/issues/724
+          deadcode: false,
+
+          // Disable the simplify plugin because it can eat some statements preceeding
+          // loops. https://github.com/babel/minify/issues/824
+          simplify: false,
+
+          // This is breaking ES6 output. https://github.com/Polymer/tools/issues/261
+          mangle: false,
+        }),
+      ],
     }),
     sizes({
       details: true,
